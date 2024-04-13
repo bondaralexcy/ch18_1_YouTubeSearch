6e9c38083cccda015d9e1f3e8772ebb4d6818eba
Alexcy <bondaralexcy@gmail.com>
2024-04-14 00:26:15 +0300

    Final commit

--- main.py
+++ main.py
@@ -5,15 +5,17 @@ from config import config
 
 
 def main():
-    api_key = os.getenv('YT_API_KEY')
+    # api_key = os.getenv('YT_API_KEY')
+    api_key = 'AIzaSyCT9YpKoMjsVWNiCwC1TeskAm_PCbEAuVM'
     channel_ids = [
-        'UC-OVMPlMA3-YCIeg4z5z23A',  # moscowpython
-        'UCwHL6WHUarjGfUM_586me8w',  # highload
-
+        # 'UC-OVMPlMA3-YCIeg4z5z23A',  # moscowpython
+        # 'UCwHL6WHUarjGfUM_586me8w',  # highload
+        'UC4NQG2u8P7t29T3aSxmGrTQ'   # metametrika
     ]
     params = config()
 
     data = get_youtube_data(api_key, channel_ids)
+    print(data)
     create_database('youtube', params)
     save_data_to_database(data, 'youtube', params)
 
